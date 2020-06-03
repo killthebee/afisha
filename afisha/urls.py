a664b6473aae7e1ebac503889fbd213073de044a
Danny <superhotfire447@yandex.ru>
2020-06-03 10:47:27 +0300

    add frontend

--- afisha/urls.py
+++ afisha/urls.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+from django.template import loader
+
+
+def render_fp(request):
+    template = loader.get_template('index.html')
+    context = {}
+    render_page = template.render(context, request)
+    return HttpResponse(render_page)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', render_fp),
 ]
